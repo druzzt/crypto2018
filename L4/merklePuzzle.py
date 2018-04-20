10234987ccd3b795b7b833f2761b579c3bb16ed2
Arkadiusz Lewandowski <arek.lewand@gmail.com>
2018-04-20 20:48:57 +0200

    descr

--- L4/merklePuzzle.py
+++ L4/merklePuzzle.py
@@ -2,7 +2,7 @@ from os import urandom
 from hashlib import sha1
 from random import shuffle, choice
 
-N = 24
+N = 17
 puzzle_size = 2 ** N
 
 def merkles_puzzle():
@@ -53,20 +53,24 @@ def solve_puzzle(puzzle):
         if sha1(pair).digest() == digest:
             return i, pair[:16], int.from_bytes(pair[16:], 'big')
 
+print('Alice sends puzzles...')
 alice_secrets, public_puzzles = merkles_puzzle()
 
+print('Bob picked one of the puzzles and is bruteforcing to find the key...')
+print('It takes a while to bruteforce.......')
 bob_time, bob_secret, public_index = solve_puzzle(choice(public_puzzles))
 
-print('Bob has secret and publishes index')
-print('key:', bob_secret)
-print('index:', public_index)
 print('steps executed:', bob_time)
+print('Bob has secret key and publishes index!')
+print('secret key:', bob_secret)
+print('index:', public_index)
 
-print('Alice has secret')
+print('Alice knows which secret key to use to contact Bob:')
 print('key:', alice_secrets[public_index])
 
 total_time, total_puzzles = 0, 0
 
+print('Now lets check how long will it take Eve to bruteforce all of the puzzles.')
 for puzzle in public_puzzles:
     adv_time, adv_key, adv_index = solve_puzzle(puzzle)
     total_time += adv_time
@@ -77,7 +81,7 @@ for puzzle in public_puzzles:
         break
 
     if total_time > bob_time * 100:
-        print('adversary failed to find secret')
+        print('adversary failed to find secret, Alice and Bob meantime had chosen new secret key')
         break
 
 print('searched puzzles:', total_puzzles, 'steps executed:', total_time)
